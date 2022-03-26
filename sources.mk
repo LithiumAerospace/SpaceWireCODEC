f57935446e6ea8ba092b5191c42cc1a8e9554f60
VA7NFH <va7nfh@gmail.com>
2022-03-26 11:21:29 -0700

    add sources.mk

---
+++ sources.mk
@@ -0,0 +1,12 @@
+SPACEWIRECODEC_SOURCES=\
+	hdl/SpaceWireCODECIPReceiverSynchronize.vhdl \
+	hdl/SpaceWireCODECIPTimeCodeControl.vhdl \
+	hdl/SpaceWireCODECIPFIFO9x64.vhdl \
+	hdl/SpaceWireCODECIPStateMachine.vhdl \
+	hdl/SpaceWireCODECIPTimer.vhdl \
+	hdl/SpaceWireCODECIPPackage.vhdl \
+	hdl/SpaceWireCODECIPLinkInterface.vhdl \
+	hdl/SpaceWireCODECIPStatisticalInformationCount.vhdl \
+	hdl/SpaceWireCODECIPTransmitter.vhdl \
+	hdl/SpaceWireCODECIPSynchronizeOnePulse.vhdl \
+	hdl/SpaceWireCODECIP.vhdl \
